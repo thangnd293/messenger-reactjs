cedd88cd2ffe8ac4e75bd4256c8484fb2a6a0660
Nguyen Dac Thang [thangnd] <thangnd@nexondv.com>
2023-03-09 14:31:35 +0700

    Add module isSameOrBefore of dayjs

--- src/config/dayjs.ts
+++ src/config/dayjs.ts
@@ -1,4 +1,6 @@
 import dayjs from 'dayjs';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
+dayjs.extend(isSameOrBefore);
 dayjs.extend(relativeTime);
